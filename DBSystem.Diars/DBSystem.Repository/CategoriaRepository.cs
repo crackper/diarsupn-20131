b2f587ebf40e40c76dd9cbb9deb0c567a3b45016
Samuel Mestanza <samuel.mestanza@hotmail.com>
2013-06-10 12:59:23 -0500

    alistando dia 2 - Semana 12

---
+++ DBSystem.Diars/DBSystem.Repository/CategoriaRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBSystem.Entities;
+
+namespace DBSystem.Repository
+{
+    public class CategoriaRepository: MasterRepository, ICategoriaRepository
+    {
+        public List<Categoria> GetAllFromCategoria()
+        {
+            return Context.Categorias.ToList();
+        }
+
+        public List<Categoria> GetFromCategoriaByCriterio(string criterio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria GetFromCategoriaById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCategoria(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DBSystem.Diars/DBSystem.Repository/ICategoriaRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBSystem.Entities;
+
+namespace DBSystem.Repository
+{
+    public interface ICategoriaRepository
+    {
+        List<Categoria> GetAllFromCategoria();
+        List<Categoria> GetFromCategoriaByCriterio(string criterio);
+        Categoria GetFromCategoriaById(Int32 id);
+        void AddCategoria(Categoria categoria);
+        void UpdateCategoria(Categoria categoria);
+        void RemoveCategoria(Int32 id);
+
+
+    }
+}
